24ea9dede860910bee34017994b3346a873bb510
Jules Frantz Stephane Loubeau <stelou29@gmail.com>
2019-05-09 13:44:29 -0400

    Initial commit

---
+++ app/src/main/java/com/td/transitiondigitale/HomeActivity.java
@@ -0,0 +1,96 @@
+package com.td.transitiondigitale;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+public class HomeActivity extends AppCompatActivity {
+
+    CarouselView carouselView;
+    Dialog myDialog;
+
+    int[] sampleImages = {R.drawable.hftic};
+    //int NUMBER_OF_PAGES = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //SliderCarousel
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+
+        //PopUp
+        myDialog = new Dialog(this);
+    }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
+
+    public void onAndroid(View view) {
+
+        TextView tvLinkAndroid;
+        Button btnRegisterAndroid;
+        Button btnShareAndroid;
+        myDialog.setContentView(R.layout.popupandroid);
+        //txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        //txtclose.setText("M");
+        btnRegisterAndroid = (Button) myDialog.findViewById(R.id.btnRegisterAndroid);
+        btnRegisterAndroid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent i = new Intent(LoginActivity.this,Register.class);
+                String url = "http://www.google.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+                myDialog.dismiss();
+            }
+        });
+
+        btnShareAndroid = (Button) myDialog.findViewById(R.id.btnShareAndroid);
+        btnShareAndroid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String shareBody = "http://www.google.com";
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, getResources().getString(R.string.share_using)));
+                myDialog.dismiss();
+            }
+        });
+
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        myDialog.show();
+    }
+
+
+    public void onIos(View view) {
+
+
+
+
+
+    }
+}
